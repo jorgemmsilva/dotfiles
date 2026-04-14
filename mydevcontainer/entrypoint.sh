113093bd4ba60bab12aeeb04ee4e12f6c9151823
Jorge Silva <jorge@syndicate.io>
2026-04-14 18:04:36 +0100

    improve nv docker inside docker hack

--- mydevcontainer/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Fix Docker socket permissions if mounted, then drop to dev user
-if [ -S /var/run/docker.sock ]; then
-    chmod 666 /var/run/docker.sock
-fi
-
-export HOME=/home/dev
-exec setpriv --reuid=dev --regid=dev --init-groups -- "$@"
