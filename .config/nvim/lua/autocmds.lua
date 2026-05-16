ae3f17e88e8c1f73512a0a4991e302d7775667c3
Jorge Silva <jorge@syndicate.io>
2026-05-16 18:15:22 +0100

    re-enable auto_refresh

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -28,34 +28,34 @@ autocmd("LspAttach", {
 ---auto-refresh files when they change underneath
 
 -- Create a variable to track the state
--- vim.g.auto_refresh_enabled = false
---
--- -- Function to toggle the behavior
--- function ToggleAutoRefresh()
---   if vim.g.auto_refresh_enabled then
---     vim.api.nvim_clear_autocmds { group = "AutoRefresh" }
---     vim.g.auto_refresh_enabled = false
---     print "Auto refresh disabled"
---   else
---     vim.o.autoread = true
---     vim.api.nvim_create_augroup("AutoRefresh", { clear = true })
---     vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
---       group = "AutoRefresh",
---       command = "if mode() != 'c' | checktime | endif",
---       pattern = "*",
---     })
---     vim.g.auto_refresh_enabled = true
---     print "Auto refresh enabled"
---   end
--- end
---
--- ToggleAutoRefresh() -- start with autorefresh enabled
---
--- vim.keymap.set("n", "<leader>rr", ToggleAutoRefresh, {
---   noremap = true,
---   silent = false,
---   desc = "Toggle auto refresh of files",
--- })
+vim.g.auto_refresh_enabled = false
+
+-- Function to toggle the behavior
+function ToggleAutoRefresh()
+  if vim.g.auto_refresh_enabled then
+    vim.api.nvim_clear_autocmds { group = "AutoRefresh" }
+    vim.g.auto_refresh_enabled = false
+    print "Auto refresh disabled"
+  else
+    vim.o.autoread = true
+    vim.api.nvim_create_augroup("AutoRefresh", { clear = true })
+    vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+      group = "AutoRefresh",
+      command = "if mode() != 'c' | checktime | endif",
+      pattern = "*",
+    })
+    vim.g.auto_refresh_enabled = true
+    print "Auto refresh enabled"
+  end
+end
+
+ToggleAutoRefresh() -- start with autorefresh enabled
+
+vim.keymap.set("n", "<leader>rr", ToggleAutoRefresh, {
+  noremap = true,
+  silent = false,
+  desc = "Toggle auto refresh of files",
+})
 
 --------------
 -- Multiple terminal fixes
